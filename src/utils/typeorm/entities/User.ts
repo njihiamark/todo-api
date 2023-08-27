85187cf3387fe472e6a793269d248e728ba6c6ce
njihiamark <njihiamark@gmail.com>
2023-08-27 03:10:26 +0300

    add active key to users table

--- src/utils/typeorm/entities/User.ts
+++ src/utils/typeorm/entities/User.ts
@@ -22,6 +22,9 @@ export class User {
   @Column({ unique: true })
   email: string;
 
+  @Column({ name: 'active' })
+  active: boolean;
+
   @CreateDateColumn({ name: 'created_at' })
   createdAt: Date;
 
